a5561e5465573ca75e21eae455c1ba159afec546
samuelsmelo77-web <samuelsmelo77@gmail.com>
2025-11-22 09:51:19 -0300

    Create calculadora.py
    Add arquivo calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+def somar(a, b):
+    return a + b
+
+def subtrair(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Erro: divisão por zero"
+    return a / b
+
+print("Calculadora carregada com sucesso!")
